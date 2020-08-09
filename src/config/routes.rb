57598ec5b258447f2baf3f03980bd9975ea0a155
Ashley Smith <ash.mash.134@gmail.com>
2020-08-09 16:56:43 +1000

    Payments now successfully complete

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :listings
   devise_for :users
   root 'home#page'
+  get "/payments/success", to: "payments#success"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
